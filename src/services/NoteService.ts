34d9cfe55713c83e4e9aa7db1320d27e2e273867
Agusfedredhunter <167997966+Agusfedredhunter@users.noreply.github.com>
2026-09-20 22:28:59 -0300

    Punto 3 completo, Augusto Fedigatti

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -41,8 +41,9 @@ export class NoteServiceImpl implements NoteService {
   getNote(id: number): Note | undefined {
     // 🔴🟢 EJERCICIO 3: ciclo completo (test + implementación).    
     //throw new Error('getNote: no implementado (Ejercicio 3)');
-    return this.repo.findById(id);
-    
+    console.log('getNote: id =', id);
+    const note = this.repo.findById(id);
+    return note;
   }
 
   updateNote(id: number, patch: NotePatch): Note | undefined {
